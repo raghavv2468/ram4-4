f482d0cacf3213965d1c21a321e72a04a56d5aee
raghavv2468 <106361352+raghavv2468@users.noreply.github.com>
2024-11-30 09:08:04 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  for_each = toset(["instance1", "instance2", "instance3"])  # Set of unique names
+
+  ami           = "ami-12345678"
+  instance_type = "t2.micro"
+  tags = {
+    Name = raghava  # Using the unique identifier as the name
+  }
+}
+
