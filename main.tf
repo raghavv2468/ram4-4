e13e13edb795f0a29fe93441713cde544a1f6d17
raghavv2468 <106361352+raghavv2468@users.noreply.github.com>
2024-11-30 10:29:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "demo" {
   for_each = toset(["instance1", "instance2", "instance3"])  # Set of unique names
 
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-0c41531b8d18cc72b"
   instance_type = "t2.micro"
   tags = {
     Name = "raghava123" # Using the unique identifier as the name
